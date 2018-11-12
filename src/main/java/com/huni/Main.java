a308a7b19562b149df456b3c37ec12538c3a9cca
Kirill <kirhany102@gmail.com>
2018-11-12 16:10:53 +0500

    Release ver 1.1.1 * Решен баг со временем

--- src/main/java/com/huni/Main.java
+++ src/main/java/com/huni/Main.java
@@ -43,16 +43,16 @@ public class Main {
         Message message = new Message();
         // Paths.get... - нужен для определения абсолютного пути jar файла, используется для удалённых компьютеров
         VKBotTimeTableReader timeTableReader = new VKBotTimeTableReader(Paths.get(".").toAbsolutePath().normalize().toString() + "/rasp.xlsx",25);
-        DateAndTimeFormatter formatter = new DateAndTimeFormatter();
 //      Задача написания расписание в заданное время
         TimerTask taskPrint = new TimerTask() {
             @Override
             public void run() {
+                DateAndTimeFormatter formatter = new DateAndTimeFormatter();
                 message.from(group);
                 message.to(137406372);
 //                Проверка на воскресенье
                 if ((Calendar.getInstance().get(Calendar.DAY_OF_WEEK)-1) != 0) {
-                    message.text(greetingFormatter(formatter.getDayTime()) + ", сегодня " + formatter.getDate() + ", текущее расписание:").send();
+                    message.text(greetingFormatter(formatter.getDayTime()) + ", сегодня " + formatter.getDate() + formatter.getTime() + ", текущее расписание:").send();
 //                Calendar.DAY_OF_WEEK - 1, т.к первым днём недели является воскресенье (так прописано в реализации Calendar), а в России понедельник.
                     Iterator it = timeTableReader.readAllRowTimeTable(Calendar.getInstance().get(Calendar.DAY_OF_WEEK)-1).iterator();
                     while(it.hasNext()) {
